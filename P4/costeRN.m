273f239a093065681ffad9a50f8743f5d8c4fc1a
nick <nick@debian>
2018-04-11 16:58:18 +0200

    update p4

--- P4/costeRN.m
+++ P4/costeRN.m
@@ -1,8 +1,8 @@
 function [J grad] = costeRN(params_rn, num_entradas, num_ocultas, num_etiquetas, X, y, lambda)
 Theta1 = reshape(params_rn(1:num_ocultas * (num_entradas + 1)), num_ocultas, (num_entradas + 1));
 Theta2 = reshape(params_rn((1 + (num_ocultas  * (num_entradas + 1))):end), num_etiquetas, (num_ocultas + 1));
-h = redn(Theta1, Theta2, X) == [1:num_etiquetas];
+h = redn(Theta1, Theta2, X);
 y = y == [1:num_etiquetas];
-J = sum(sum(-y * log(h) - (1 - y) * log(1 - h))) / rows(X);
+J = sum(sum((-y).*log(h) - (1-y).*log(1-h))) / rows(X) + (lambda / (2 * rows(X))) * (sum(sum(pow2(Theta1))) + sum(sum(pow2(Theta2))));
 grad = 0;
 endfunction
--- P4/misc1.m
+++ P4/misc1.m
@@ -1,2 +1,4 @@
-params_rn = Theta1(:) ; Theta2(:);
-costeRN(params_rn, 400, 25, 10, X, y, 0.1);
+load("ex4data1.mat");
+load("ex4weights.mat");
+params_rn = [Theta1(:) ; Theta2(:)];
+costeRN(params_rn, 400, 25, 10, X, y, 0.1)
