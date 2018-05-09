a58554ca658b96842647b32e8e4c161eb69469a0
nicknas <nicknas@github.com>
2018-05-09 23:15:07 +0200

    Updated p7 full

--- p7/computeCentroids.m
+++ p7/computeCentroids.m
@@ -1,18 +1,22 @@
+# Funcion que vuelve a calcular aquellos centroides óptimos para cada ejemplo
 function centroids = computeCentroids(X, idx, K)
 
 [m n] = size(X);
 
 centroids = zeros(K, n);
 
+# Para cada fila de centroide, buscamos aquellos ejemplos que concuerden con los indices guardados en idx
 for k = 1:K
   num_k = 0;
   sum = zeros(n, 1)';
   for i = 1:m
+    # Si concuerda, realizamos el sumatorio de variables para ese ejemplo 
     if ( idx(i) == k )
       sum = sum + X(i, :);
       num_k = num_k + 1;
     end
   end
+  # Al final de cada fila realizamos la media de todas aquellas variables concordantes
   centroids(k, :) = (sum/num_k);
 end
 end
--- p7/findClosestCentroids.m
+++ p7/findClosestCentroids.m
@@ -1,3 +1,4 @@
+%Funcion que calcula aquel centroide mas cercano a cada ejemplo de X
 function idx = findClosestCentroids(X, centroids)
   %Calculamos K
   K = rows(centroids);
@@ -9,9 +10,11 @@ function idx = findClosestCentroids(X, centroids)
     min_ci = inf;
     min_idx = 1;
     xi = X(i, :);
+    %Para cada fila de centroide, se realiza el cuadrado de las diferencias según la fórmula
     for j = 1:K
       uj = centroids(j, :);
       ci = abs(xi - uj) * abs(xi - uj)';
+      %Si de momento es la minima diferencia, se guarda el indice como posible minimo
       if (ci < min_ci)
       {
         min_ci = ci;
